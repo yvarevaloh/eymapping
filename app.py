c5413fa791520ab68ed0b685ab3ce76b35be5078
Yeimy Viviana Arevalo Hernandez <yeimy.arevalo@globant.com>
2019-08-08 17:02:22 -0500

    new requirements

--- app.py
+++ app.py
@@ -4,40 +4,34 @@
 # In[ ]:
 
 from flask import Flask, request
-from nltk.tokenize import sent_tokenize, word_tokenize
+#from nltk.tokenize import sent_tokenize, word_tokenize
 #import spacy
 #from spacy import displacy
-from collections import Counter
+#from collections import Counter
 #import en_core_web_sm
 #nlp = en_core_web_sm.load()
 
-import numpy as np
+#import numpy as np
 from flask import jsonify
 from flask import request
-import _pickle as cPickle
+#import _pickle as cPickle
 import pandas as pd
 
 #from nltk.tokenize import sent_tokenize, word_tokenize
-from tablib import Dataset
-import json
-import os
-import numpy as np
-import re
-import random
-import csv
-
-#import nltk
-#nltk.download('stopwords')
-#from nltk.corpus import stopwords
+#from tablib import Dataset
+#import json
+#import os
+#import numpy as np
+#import re
+#import random
+#import csv
 
 import tensorflow as tf
-import tensorflow_hub as hub
-import keras
-from keras.models import Model
-from keras.models import load_model
-from keras.layers import Input, Dense, Lambda, Dropout, BatchNormalization
-
-
+#import tensorflow_hub as hub
+#import keras
+#from keras.models import Model
+#from keras.models import load_model
+#from keras.layers import Input, Dense, Lambda, Dropout, BatchNormalization
 
 
 app = Flask(__name__)
